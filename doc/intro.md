c4573c1a35a071daba07c8c541d2a437371cd713
Andy Chambers <andy.chambers@fundingcircle.com>
2016-09-18 22:11:40 -0700

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kafka.test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kafka/test_test.clj
@@ -0,0 +1,7 @@
+(ns kafka.test-test
+  (:require [clojure.test :refer :all]
+            [kafka.test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
