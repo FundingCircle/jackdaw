535106279f25de33c911f74ab1bbeb76650926fc
Matthias Margush <matthias.margush@fundingcircle.com>
2016-09-22 11:08:21 -0700

    circle ci

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kstreams-common "0.1.0"
+(defproject kstreams-common "0.1.0-SNAPSHOT"
   :description "Kafka streams clojure wrapper"
   :url "https://github.com/FundingCircle/kstreams-common"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,11 @@
   :dependencies [[org.apache.kafka/kafka-streams "0.10.0.0-cp1"]
                  [org.clojure/clojure "1.8.0"]]
   :repositories {"confluent" {:url "http://packages.confluent.io/maven/"}
+                 "snapshots" {:url "https://fundingcircle.artifactoryonline.com/fundingcircle/libs-snapshot-local"
+                              :username [:gpg :env/artifactory_user]
+                              :password [:gpg :env/artifactory_password]
+                              :sign-releases false}
                  "releases" {:url "https://fundingcircle.artifactoryonline.com/fundingcircle/libs-release-local"
-                             :sign-releases false
                              :username [:gpg :env/artifactory_user]
-                             :password [:gpg :env/artifactory_password]}})
+                             :password [:gpg :env/artifactory_password]
+                             :sign-releases false}})
