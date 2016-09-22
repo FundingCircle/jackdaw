72ef12e2324a38381ba39cacd7c0bcb1c8fd017b
Tianxiang Xiong <tianxiang.xiong@fundingcircle.com>
2016-09-22 11:14:38 -0700

    Add `circle.yml` and repos

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kafka-serdes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Serializers/deserializers for Kafka"
+  :url "https://github.com/FundingCircle/kafka-serdes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :local-repo ".repo"
@@ -11,5 +11,12 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/test.check "0.9.0"]]
-  :repositories  {"confluent"
-                  {:url "http://packages.confluent.io/maven/"}})
+  :repositories  {"confluent" {:url "http://packages.confluent.io/maven/"}
+                  "snapshots" {:url "https://fundingcircle.artifactoryonline.com/fundingcircle/libs-snapshot-local"
+                               :username [:gpg :env/artifactory_user]
+                               :password [:gpg :env/artifactory_password]
+                               :sign-releases false}
+                  "releases" {:url "https://fundingcircle.artifactoryonline.com/fundingcircle/libs-release-local"
+                              :username [:gpg :env/artifactory_user]
+                              :password [:gpg :env/artifactory_password]
+                              :sign-releases false}})
