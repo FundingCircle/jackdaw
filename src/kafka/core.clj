0c11b35118435fd0581de41b7fda9fdeb83fc2e0
Andy Chambers <andy.chambers@fundingcircle.com>
2016-10-02 23:37:53 -0700

    Add wrappers for callbacks, consumer-records, and producer metadata

--- src/kafka/core.clj
+++ src/kafka/core.clj
@@ -1,7 +1,12 @@
 (ns kafka.core
   "Clojure wrapper to kafka consumers/producers"
-  (:import org.apache.kafka.clients.consumer.KafkaConsumer
-           org.apache.kafka.clients.producer.KafkaProducer))
+  (:require
+   [clojure.tools.logging :as log])
+  (:import
+   (org.apache.kafka.clients.consumer KafkaConsumer ConsumerRecord)
+   (org.apache.kafka.clients.producer KafkaProducer ProducerRecord Callback)
+   (java.util.concurrent LinkedBlockingQueue)
+   (clojure.lang Reflector)))
 
 (defn producer
   "Return a KafkaProducer with the supplied properties"
@@ -19,29 +24,39 @@
   ([props key-deserializer value-deserializer]
    (KafkaConsumer. props key-deserializer value-deserializer)))
 
-(defn logs
-  "Return a lazy-seq with all records delivered by kafka to the supplied consumer.
+(defn callback
+  "Build a kafka producer callback function out of a normal clojure one
+   The function should expect two parameters, the first being the record
+   metadata, the second being an exception if there was one. The function
+   should check for an exception and handle it appropriately."
+  [on-completion]
+  (reify Callback
+    (onCompletion [this record-metadata exception]
+      (on-completion record-metadata exception))))
 
-   Provided as a convenience so we don't have to manually poll a consumer.
+(defn select-methods
+  "Like `select-keys` but instead builds a map by invoking the named java methods
+   for the corresponding keys"
+  [object methods]
+  (let [jget (fn [m]
+               (Reflector/invokeInstanceMethod object
+                                               (str (name m)) (into-array [])))]
+    (apply hash-map (->> methods
+                         (map (fn [m]
+                                [m (jget m)]))
+                         (mapcat identity)))))
 
-   An optional latch may be used to ensure no poll occurs after releasing
-   the latch. This can be useful when the consumer may be closed in another
-   thread."
-  ([consumer]
-   (logs nil consumer 1000))
+(defn metadata
+  "Clojurize the metadata returned from producing a kafka record"
+  [record-meta]
+  (select-methods record-meta
+                  [:checksum :offset :partition
+                   :serializedKeySize :serializedValueSize
+                   :timestamp :topic :toString]))
 
-  ([consumer latch]
-   (logs latch consumer 1000))
-
-  ([consumer latch poll-freq-ms]
-   (letfn [(lazy-iterate [iterator]
-             (lazy-seq
-              (when (.hasNext iterator)
-                (cons (.next iterator) (lazy-iterate iterator)))))]
-
-     (lazy-seq
-      (when-let [records (when (and latch (pos? (.getCount latch)))
-                           (locking consumer (.poll consumer poll-freq-ms)))]
-        (concat (lazy-iterate
-                 (.iterator records))
-                (logs consumer latch poll-freq-ms)))))))
+(defn record [consumer-record]
+  (select-methods consumer-record
+                  [:checksum :key :offset :partition
+                   :serializedKeySize :serializedValueSize
+                   :timestamp :timestampType
+                   :topic :toString :value]))
