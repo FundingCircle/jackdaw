33c7fc4558b151772f8fef26b2ae24567c1cde21
Andy Chambers <andy.chambers@fundingcircle.com>
2016-10-04 12:36:38 -0700

    Use fundingcircle maven group; Build candidate branches from circle

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject kafka.client "0.1.0-SNAPSHOT"
+(defproject fundingcircle/kafka.client "0.1.0-SNAPSHOT"
   :description "No frills Clojure wrapper around core kafka APIs"
   :url "http://github.com/FundingCircle/kafka.client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-
                  [com.101tec/zkclient "0.8"]
                  [org.apache.kafka/kafka_2.11 "0.10.0.1"]
                  [org.apache.kafka/kafka-clients "0.10.0.1"]]
