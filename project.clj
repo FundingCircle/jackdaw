0c8dc9581b641fbaf5c8f71c0a867dc4d5eb1e1b
Matthias Margush <matthias.margush@fundingcircle.com>
2016-11-18 11:58:00 -0800

    Incorporate topic.metadata

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/FundingCircle/kstreams-common"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :local-repo ".repo"
   :dependencies [[org.apache.kafka/kafka-streams "0.10.0.0-cp1"]
                  [org.clojure/clojure "1.8.0"]]
 
