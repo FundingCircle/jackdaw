a14fe444df9fecc1a2ab3124bc2f0e3224114edc
Matthias Margush <matthias.margush@fundingcircle.com>
2016-11-29 10:52:06 -0800

    Version 0.5.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/kafka.serdes "0.5.3-SNAPSHOT"
+(defproject fundingcircle/kafka.serdes "0.5.3"
   :description "Serializers/deserializers for Kafka"
   :url "https://github.com/FundingCircle/kafka-serdes"
   :license {:name "Eclipse Public License"
