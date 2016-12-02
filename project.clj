8f11987241c5f9b0eca1a4474dcdf26025e8ebd7
Matthias Margush <matthias.margush@fundingcircle.com>
2016-12-02 10:32:00 -0800

    Version 0.4.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/kafka.streams "0.4.2-SNAPSHOT"
+(defproject fundingcircle/kafka.streams "0.4.2"
   :description "Kafka streams clojure wrapper"
   :url "https://github.com/FundingCircle/kstreams-common"
   :license {:name "Eclipse Public License"
