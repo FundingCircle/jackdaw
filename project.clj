6fe45b692cef89030e8b56197db1078910af865f
Matthias Margush <matthias.margush@fundingcircle.com>
2017-01-18 11:08:14 -0800

    Upgrade to kafka 10.1

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
                  [org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [com.101tec/zkclient "0.8" :exclusions [log4j org.slf4j/slf4j-log4j12]]
-                 [org.apache.kafka/kafka_2.11 "0.10.0.1" :exclusions [log4j org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api
+                 [org.apache.kafka/kafka_2.11 "0.10.1.0-cp2" :exclusions [log4j org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api
                                                                       com.fasterxml.jackson.core/jackson-annotations
                                                                       com.fasterxml.jackson.core/jackson-core]]
-                 [org.apache.kafka/kafka-clients "0.10.0.1" :exclusions [log4j org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api
+                 [org.apache.kafka/kafka-clients "0.10.1.0-cp2" :exclusions [log4j org.slf4j/slf4j-log4j12 org.slf4j/slf4j-api
                                                                          com.fasterxml.jackson.core/jackson-annotations
                                                                          com.fasterxml.jackson.core/jackson-core]]]
 
@@ -31,4 +31,5 @@
    "releases" {:url "https://fundingcircle.artifactoryonline.com/fundingcircle/libs-release-local"
                :username [:gpg :env/artifactory_user]
                :password [:gpg :env/artifactory_password]
-               :sign-releases false}})
+               :sign-releases false}
+   "confluent" {:url "http://packages.confluent.io/maven/"}})
