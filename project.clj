f9ee47c10742ae574c6d1788380e59696f787b8f
Tianxiang Xiong <tianxiang.xiong@fundingcircle.com>
2017-03-25 00:53:26 -0700

    Version 0.10.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/kafka.serdes "0.10.2-SNAPSHOT"
+(defproject fundingcircle/kafka.serdes "0.10.2"
   :description "Serializers/deserializers for Kafka"
   :url "https://github.com/FundingCircle/kafka-serdes"
   :license {:name "Eclipse Public License"
