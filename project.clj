e85faf73998ef0cc96838fdaa8e0a0f00f2ac506
Matthias Margush <matthias.margush@fundingcircle.com>
2017-07-03 14:26:37 -0700

    added codox generation to project.clj
    ```
    lein sub codox
    open jackdaw-client/target/doc/index.html
    open jackdaw-serdes/target/doc/index.html
    open jackdaw-streams/target/doc/index.html
    ```

--- project.clj
+++ project.clj
@@ -1,15 +1,17 @@
 (defproject fundingcircle/jackdaw "0.1.0-SNAPSHOT"
   :description "No frills Clojure wrapper around Apache Kafka APIs"
   :url "http://github.com/FundingCircle/jackdaw"
-  :dependencies [[org.apache.kafka/kafka-clients "0.10.2.1"
-                  :exclusions [com.fasterxml.jackson.core/jackson-annotations
-                               com.fasterxml.jackson.core/jackson-core]]
-                 [org.apache.kafka/kafka_2.11 "0.10.2.1"
-                  :exclusions [com.fasterxml.jackson.core/jackson-annotations
-                               com.fasterxml.jackson.core/jackson-core]]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]]
-  :plugins [[lein-sub "0.3.0"]]
+  :dependencies [[fundingcircle/jackdaw-client "0.1.0-SNAPSHOT"]
+                 [fundingcircle/jackdaw-serdes "0.1.0-SNAPSHOT"]
+                 [fundingcircle/jackdaw-streams "0.1.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-codox "0.10.3"]
+            [lein-sub "0.3.0"]]
   :sub ["jackdaw-client"
         "jackdaw-serdes"
-        "jackdaw-streams"])
+        "jackdaw-streams"]
+  :codox {:output-path "codox"
+          :source-uri "http://github.com/fundingcircle/jackdaw/blob/{version}/{filepath}#L{line}"
+          :source-paths ["jackdaw-client/src"
+                         "jackdaw-serdes/src"
+                         "jackdaw-streams/src"]})
