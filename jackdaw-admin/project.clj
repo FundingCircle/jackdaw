3efe8c00eb2208c56b68a691bed4d8048c8be27d
Chris Devine <christopher.devine@fundingcircle.com>
2017-07-06 12:58:54 -0700

    Removes unnecessary dependencies, moves others to test profile

--- jackdaw-admin/project.clj
+++ jackdaw-admin/project.clj
@@ -3,21 +3,16 @@
   :target-path "target/%s"
   :uberjar-name "kafka.admin.jar"
   :dependencies [[org.clojure/tools.logging "_"]
-                 [org.apache.kafka/kafka_2.11 "_"]
-                 [environ "1.1.0" :exclusions [org.clojure/clojure]]
-                 [boot-environ "1.1.0" :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [reply "0.3.7" :exclusions [commons-io commons-codec]]]
-  :plugins [[lein-environ "1.1.0"]
-            [lein-marginalia "0.9.0"]
-            [lein-modules "0.3.11"]]
-
+                 [org.apache.kafka/kafka_2.11 "_"]]
+  :plugins [[lein-modules "0.3.11"]]
   :profiles {:dev [:project/dev :kafka]
              :test [:project/test :kafka]
              :project/test {:resource-paths ["test/resources"]
-                           :env {;; confluent packages have ports xxx9 to prevent conflict with local env
-                                 :zookeeper-address "127.0.0.1:2189"
-                                 :bootstrap-servers "127.0.0.1:9099"}}
+                            :env {;; confluent packages have ports xxx9 to prevent conflict with local env
+                                  :zookeeper-address "127.0.0.1:2189"
+                                  :bootstrap-servers "127.0.0.1:9099"}
+                            :plugins [[lein-environ "1.1.0"]]
+                            :dependencies [[environ "1.1.0" :exclusions [org.clojure/clojure]]]}
              :project/dev {:env {:zookeeper-address "127.0.0.1:2181"
                                  :bootstrap-servers "127.0.0.1:9092"}}
              :kafka {:dependencies [[fundingcircle/jackdaw-test "0.1.0-SNAPSHOT"]
