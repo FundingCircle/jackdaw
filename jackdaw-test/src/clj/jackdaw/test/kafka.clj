bb6b0d1642e0964797c8e92aab5fe36ae916c1ca
Reid 'arrdem' McKenzie <rmckenzie@twitter.com>
2017-12-12 17:54:37 -0800

    Write the meta.properties file that Kafka complains about not having

--- jackdaw-test/src/clj/jackdaw/test/kafka.clj
+++ jackdaw-test/src/clj/jackdaw/test/kafka.clj
@@ -1,9 +1,12 @@
 (ns jackdaw.test.kafka
   (:require
    [clojure.java.io :as io]
+   [clojure.string :as string]
    [jackdaw.test.fs :as fs])
   (:import
-   (kafka.server KafkaConfig
+   (kafka.server BrokerMetadata
+                 KafkaConfig
+                 BrokerMetadataCheckpoint
                  KafkaServerStartable)))
 
 (defn start!
@@ -12,7 +15,13 @@
    Returns a map containing the broker instance itself and a latch
    that waits until the broker is shutdown"
   [{:keys [config]}]
-  (let [props (java.util.Properties.) ]
+  (let [props           (java.util.Properties.)
+        checkpoint-file (io/file (get config "log.dirs") "meta.properties")
+        broker-id       (Integer/parseInt (get config "broker.id"))]
+    (io/make-parents checkpoint-file)
+    (. (BrokerMetadataCheckpoint. checkpoint-file)
+       (write (BrokerMetadata. broker-id)))
+
     (.putAll props config)
     (let [broker (-> props
                      (KafkaConfig.)
