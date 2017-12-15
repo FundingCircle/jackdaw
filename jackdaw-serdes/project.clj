e4c7edd13025c1d2bba2bf01552082b91492cc04
Allen Rohner <allen.rohner@fundingcircle.com>
2017-12-15 15:02:08 -0600

    Version 0.3.4

--- jackdaw-serdes/project.clj
+++ jackdaw-serdes/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-serdes "0.3.4-SNAPSHOT"
+(defproject fundingcircle/jackdaw-serdes "0.3.4"
   :description "Serializers/deserializers for Kafka"
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[danlentz/clj-uuid "0.1.7"]
