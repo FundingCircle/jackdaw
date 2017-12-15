15e4386e630bf7a761a19a5eec6baa21f0f6380d
Allen Rohner <allen.rohner@fundingcircle.com>
2017-12-15 15:04:58 -0600

    Version 0.3.5-SNAPSHOT

--- jackdaw-streams/project.clj
+++ jackdaw-streams/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-streams "0.3.4"
+(defproject fundingcircle/jackdaw-streams "0.3.5-SNAPSHOT"
   :description "Kafka streams clojure wrapper"
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[org.apache.kafka/kafka-streams "_"]]
--- jackdaw-test/project.clj
+++ jackdaw-test/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-test "0.3.1"
+(defproject fundingcircle/jackdaw-test "0.3.5-SNAPSHOT"
   :description "Test fixtures for Kafka, Zookeeper, and Confluent Schema Registry"
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
