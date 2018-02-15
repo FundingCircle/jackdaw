a4d4cf2bb1c8ffa9d6fe8177fd0745497784d192
JPol <jpol@fundingcircle.com>
2018-02-15 15:00:18 -0800

    Version 0.3.7-SNAPSHOT

--- jackdaw-admin/project.clj
+++ jackdaw-admin/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-admin "0.3.6"
+(defproject fundingcircle/jackdaw-admin "0.3.7-SNAPSHOT"
   :description "Tools for kafka administration"
   :dependencies [[org.clojure/tools.logging "_"]
                  [org.apache.kafka/kafka_2.11 "_"]]
--- jackdaw-serdes/project.clj
+++ jackdaw-serdes/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-serdes "0.3.6"
+(defproject fundingcircle/jackdaw-serdes "0.3.7-SNAPSHOT"
   :description "Serializers/deserializers for Kafka"
   :plugins [[lein-modules "0.3.11"]]
   :dependencies [[danlentz/clj-uuid "0.1.7"]
