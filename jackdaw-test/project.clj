b094ece9266872022d0cfef36de7031760ac1d4f
Allen Rohner <allen.rohner@fundingcircle.com>
2018-02-27 17:00:52 -0600

    Version 0.3.10

--- jackdaw-test/project.clj
+++ jackdaw-test/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-test "0.3.10-SNAPSHOT"
+(defproject fundingcircle/jackdaw-test "0.3.10"
   :description "Test fixtures for Kafka, Zookeeper, and Confluent Schema Registry"
   :source-paths      ["src/clj"]
   :java-source-paths ["src/java"]
