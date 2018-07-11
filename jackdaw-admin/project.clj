d1dfdd98ed2d899f09313d896d95f9f77cffd8a4
Morgan Astra <m@morganastra.me>
2018-07-11 15:24:11 -0700

    Version 0.4.0

--- jackdaw-admin/project.clj
+++ jackdaw-admin/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-admin "0.3.18-SNAPSHOT"
+(defproject fundingcircle/jackdaw-admin "0.3.18"
   :description "Tools for kafka administration"
 
   :dependencies [[org.clojure/tools.logging "_"]
--- jackdaw-streams/project.clj
+++ jackdaw-streams/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-streams "0.3.18-SNAPSHOT"
+(defproject fundingcircle/jackdaw-streams "0.3.18"
   :description "Kafka streams clojure wrapper"
 
   :plugins [[lein-modules "0.3.11"]]
