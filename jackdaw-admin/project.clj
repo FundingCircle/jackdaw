6ebe1e8576f073dd0a4ab3d34b8679723ee8fa42
Morgan Astra <m@morganastra.me>
2018-07-13 17:07:44 -0700

    Version 0.3.20

--- jackdaw-admin/project.clj
+++ jackdaw-admin/project.clj
@@ -1,4 +1,4 @@
-(defproject fundingcircle/jackdaw-admin "0.3.20-SNAPSHOT"
+(defproject fundingcircle/jackdaw-admin "0.3.20"
   :description "Tools for kafka administration"
 
   :dependencies [[org.clojure/tools.logging "_"]
