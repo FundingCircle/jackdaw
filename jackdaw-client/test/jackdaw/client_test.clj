bd45952878186c631b630b48fd8b53c2ccefba3b
Justin Glenn Smith <justin.smith@fundingcircle.com>
2018-09-26 11:19:20 -0700

    dead code removal

--- jackdaw-client/test/jackdaw/client_test.clj
+++ jackdaw-client/test/jackdaw/client_test.clj
@@ -6,20 +6,6 @@
            [org.apache.kafka.clients.producer Producer]
            org.apache.kafka.common.TopicPartition))
 
-(def producer-config
-  {"bootstrap.servers" "localhost:9092"
-   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
-   "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"})
-
-(def consumer-config
-  {"bootstrap.servers"     "localhost:9092"
-   "group.id"              "test"
-   "key.deserializer"      "org.apache.kafka.common.serialization.StringDeserializer"
-   "value.deserializer"    "org.apache.kafka.common.serialization.StringDeserializer"
-   "metadata.max.age.ms"   "1000" ;; usually this is 5 minutes
-   "auto.offset.reset"     "earliest"
-   "enable.auto.commit"    "true"})
-
 (deftest callback-test
   (testing "producer callbacks"
     (testing "success"
