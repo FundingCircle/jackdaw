1aba18a99132c02891af67b4d4fa40f81399c3dc
Matt Searle <matthew.searle@fundingcircle.com>
2018-11-05 12:36:21 +0000

    Delete examples as these were already added in ./examples

--- jackdaw-examples/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject fundingcircle/jackdaw-examples "0.3.27-SNAPSHOT"
-  :description "Example Jackdaw uses"
-
-  :plugins [[fundingcircle/lein-modules "[0.3.0,0.4.0)"]]
-
-  :dependencies [[fundingcircle/jackdaw-streams "_"]
-                 [fundingcircle/jackdaw-serdes "_"]])
