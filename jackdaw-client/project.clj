46bea0c0cc77ca2b1553c78213de5eb1c8abd476
Morgan Astra <m@morganastra.me>
2018-11-09 16:16:45 -0800

    Move files out of modules

--- jackdaw-client/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject fundingcircle/jackdaw-client "0.3.27-SNAPSHOT"
-  :description "Clojure wrapper for Apache Kafka Producer and Consumer APIs"
-
-  :plugins [[fundingcircle/lein-modules "[0.3.0,0.4.0)"]]
-
-  :dependencies [[org.clojure/tools.logging "_"]
-                 [org.apache.kafka/kafka-clients "_"]
-                 [org.apache.kafka/kafka_2.11 "_"]
-                 [org.apache.kafka/kafka-streams "_"]])
