533b79f29e302090609fa8a6f7315b10b08bcdeb
Charles Reese <charles.reese@fundingcircle.com>
2018-11-26 13:49:49 -0800

    Tweak project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject fundingcircle/jackdaw "0.4.0-SNAPSHOT"
-  :description "Clojure library for the Apache Kafka distributed streaming platform."
+  :description "A Clojure library for the Apache Kafka distributed streaming platform."
 
   :dependencies [[clj-time "0.13.0"]
                  [com.taoensso/nippy "2.12.2"]
