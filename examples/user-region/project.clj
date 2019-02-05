991f79911c395377060bac980aaa1e2c618585bc
astelvida <sevda.m.anefi@gmail.com>
2019-02-05 17:57:00 +0000

    rename to user-region and add readme

--- examples/user-region/project.clj
+++ examples/user-region/project.clj
@@ -1,6 +1,6 @@
-(defproject user-events "0.1.0-SNAPSHOT"
+(defproject user-region "0.1.0-SNAPSHOT"
   :description "Demonstrates group-by operations and aggregations on KTable by computing
-  user-count per region"
+  the number of users per region"
   :dependencies [[fundingcircle/jackdaw "0.4.3"]
                  [org.apache.kafka/kafka-streams "2.0.0"]
                  [org.apache.kafka/kafka-streams-test-utils "2.0.0"]
@@ -8,6 +8,5 @@
                  [org.clojure/tools.logging "0.4.1"]]
   :repositories [["confluent" "https://packages.confluent.io/maven/"]]
   :source-paths ["src" "dev" "../dev"]
-  :main ^:skip-aot user-events.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :main ^:skip-aot user-region.core
+  :target-path "target/%s")
