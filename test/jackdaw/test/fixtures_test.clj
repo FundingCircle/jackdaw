edb5b106410516fb8464ce02d48d87a6d74edd25
Andy Chambers <andy.chambers@fundingcircle.com>
2019-08-24 15:17:25 +0100

    Make reset-application-fixture support optional parameters

--- test/jackdaw/test/fixtures_test.clj
+++ test/jackdaw/test/fixtures_test.clj
@@ -32,4 +32,56 @@
     (with-open [client (AdminClient/create kafka-config)]
       (is (topic-exists? client topic-foo)))))
 
+(defn test-resetter
+  ""
+  {:style/indent 1}
+  [{:keys [app-config reset-params reset-fn]} assertion-fn]
+  (let [reset-args (atom [])
+        error-data (atom {})
+        test-fn (fn []
+                  (is true "fake test function"))
+        fix-fn (reset-application-fixture app-config reset-params
+                                          (partial reset-fn reset-args))]
 
+    ;; invoke the reset-application fixture with the sample test-fn
+    (try
+      (fix-fn test-fn)
+      (catch Exception e
+        (reset! error-data (ex-data e))))
+
+    (assertion-fn {:resetter (first @reset-args)
+                   :reset-args (second @reset-args)
+                   :error-data @error-data})))
+
+(deftest test-reset-application-fixture
+  (test-resetter {:app-config {"application.id" "yolo"
+                               "bootstrap.servers" "kafka.test:9092"}
+                  :reset-params ["--foo" "foo"
+                                 "--bar" "bar"]
+                  :reset-fn (fn [reset-args rt args]
+                              (reset! reset-args [rt args])
+                              0)}
+    (fn [{:keys [resetter reset-args error-data]}]
+      (is (instance? kafka.tools.StreamsResetter resetter))
+      (is (= ["--application-id" "yolo"
+              "--bootstrap-servers" "kafka.test:9092"
+              "--foo" "foo"
+              "--bar" "bar"]
+             reset-args))
+      (is (empty? error-data)))))
+
+(deftest test-reset-application-fixture-failure
+  (test-resetter {:app-config {"application.id" "yolo"
+                               "bootstrap.servers" "kafka.test:9092"}
+                  :reset-params ["--foo" "foo"
+                                 "--bar" "bar"]
+                  :reset-fn (fn [reset-args rt args]
+                              (reset! reset-args [rt args])
+                              (.write *err* "helpful error message\n")
+                              (.write *out* "essential application info\n")
+                              1)}
+    (fn [{:keys [resetter reset-args error-data]}]
+      (is (instance? kafka.tools.StreamsResetter resetter))
+      (is (= 1 (:status error-data)))
+      (is (= "helpful error message\n" (:err error-data)))
+      (is (= "essential application info\n" (:out error-data))))))
