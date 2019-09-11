bebb40519c8f43e3501fc0074d7cbb7deb1b4b8d
Charles Reese <charles.reese@fundingcircle.com>
2019-09-11 12:53:41 -0700

    Move new EDN serde to separate namespace

---
+++ src/jackdaw/serdes/edn2.clj
@@ -0,0 +1,54 @@
+(ns jackdaw.serdes.edn2
+  "Implements an EDN SerDes (Serializer/Deserializer)."
+  (:require [clojure.edn]
+            [jackdaw.serdes.fn :as jsfn])
+  (:import java.nio.charset.StandardCharsets
+           org.apache.kafka.common.serialization.Serde
+           org.apache.kafka.common.serialization.Serdes)
+  (:gen-class
+   :implements [org.apache.kafka.common.serialization.Serde]
+   :prefix "EdnSerde-"
+   :name jackdaw.serdes.EdnSerde))
+
+(set! *warn-on-reflection* true)
+
+(defn to-bytes
+  "Converts a string to a byte array."
+  [data]
+  (.getBytes ^String data StandardCharsets/UTF_8))
+
+(defn- from-bytes
+  "Converts a byte array to a string."
+  [^bytes data]
+  (String. data StandardCharsets/UTF_8))
+
+(defn edn-serializer
+  "Returns an EDN serializer."
+  []
+  (jsfn/new-serializer {:serialize (fn [_ _ data]
+                                     (when data
+                                       (to-bytes
+                                        (binding [*print-length* false
+                                                  *print-level* false]
+                                          (pr-str data)))))}))
+
+(defn edn-deserializer
+  "Returns an EDN deserializer."
+  ([]
+   (edn-deserializer {}))
+  ([opts]
+   (let [opts (into {} opts)]
+     (jsfn/new-deserializer {:deserialize (fn [_ _ data]
+                                            (->> (from-bytes data)
+                                                 (clojure.edn/read-string opts)))}))))
+
+(def EdnSerde-configure
+  (constantly nil))
+
+(defn EdnSerde-serializer
+  [& _]
+  (edn-serializer))
+
+(defn EdnSerde-deserializer
+  [& _]
+  (edn-deserializer))
--- test/jackdaw/serdes/edn2_test.clj
+++ test/jackdaw/serdes/edn2_test.clj
@@ -1,31 +1,31 @@
-(ns jackdaw.serdes-test
+(ns jackdaw.serdes.edn2-test
   (:require [clojure.spec.alpha :as s]
             [clojure.test :refer :all]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
-            [jackdaw.serdes :as js]))
+            [jackdaw.serdes.edn2 :as jse]))
 
 (defspec edn-roundtrip-test 20
   (testing "EDN data is the same after serialization and deserialization."
     (prop/for-all [x (gen/fmap #(apply str %) (gen/vector gen/char-alpha 100))]
-      (is (= x (->> (.serialize (js/edn-serializer) nil x)
-                    (.deserialize (js/edn-deserializer) nil)))))))
+      (is (= x (->> (.serialize (jse/edn-serializer) nil x)
+                    (.deserialize (jse/edn-deserializer) nil)))))))
 
 (defspec edn-reverse-roundtrip-test 20
   (testing "EDN data is the same after deserialization and serialization."
     (prop/for-all [x (gen/fmap #(apply str %) (gen/vector gen/char-alpha 100))]
-      (let [bytes (.serialize (js/edn-serializer) nil x)]
+      (let [bytes (.serialize (jse/edn-serializer) nil x)]
         (is (= (seq bytes)
-               (seq (->> (.deserialize (js/edn-deserializer) nil bytes)
-                         (.serialize (js/edn-serializer) nil)))))))))
+               (seq (->> (.deserialize (jse/edn-deserializer) nil bytes)
+                         (.serialize (jse/edn-serializer) nil)))))))))
 
 (defspec edn-print-length-test 20
   (testing "EDN data is the same after serialization and deserialization with *print-length*."
     (binding [*print-length* 100]
       (prop/for-all [x (gen/vector gen/int (inc *print-length*))]
-        (is (= x (->> (.serialize (js/edn-serializer) nil x)
-                      (.deserialize (js/edn-deserializer) nil))))))))
+        (is (= x (->> (.serialize (jse/edn-serializer) nil x)
+                      (.deserialize (jse/edn-deserializer) nil))))))))
 
 (defmethod print-method java.net.URI
   [obj writer]
@@ -36,5 +36,5 @@
   (testing "custom EDN reader"
     (let [opts {:readers {'jackdaw/uri #(java.net.URI. %)}}]
       (prop/for-all [x (s/gen uri?)]
-        (is (= x (->> (.serialize (js/edn-serializer) nil x)
-                      (.deserialize (js/edn-deserializer opts) nil))))))))
+        (is (= x (->> (.serialize (jse/edn-serializer) nil x)
+                      (.deserialize (jse/edn-deserializer opts) nil))))))))
