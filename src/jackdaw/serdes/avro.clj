33ac94d93499a461783afd8767540927d1cca422
Daniel Carleton <daniel.carleton@reifyhealth.com>
2020-06-15 09:21:05 -0700

    fix for #258

--- src/jackdaw/serdes/avro.clj
+++ src/jackdaw/serdes/avro.clj
@@ -389,15 +389,17 @@
 (defrecord RecordType [^Schema schema field->schema+coercion]
   SchemaCoercion
   (match-clj? [_ clj-map]
-    (let [[_ unknown-fields _] (clojure.data/diff (set (keys field->schema+coercion))
-                                                  (set (keys clj-map)))]
-      (and (every? (fn [[field-key [^Schema$Field field field-coercion]]]
-                     (let [field-value (get clj-map field-key ::missing)]
-                       (if (= field-value ::missing)
-                         (.defaultValue field)
-                         (match-clj? field-coercion field-value))))
-                   field->schema+coercion)
-           (empty? unknown-fields))))
+    (if (map? clj-map)
+      (let [[_ unknown-fields _] (clojure.data/diff (set (keys field->schema+coercion))
+                                                    (set (keys clj-map)))]
+        (and (every? (fn [[field-key [^Schema$Field field field-coercion]]]
+                       (let [field-value (get clj-map field-key ::missing)]
+                         (if (= field-value ::missing)
+                           (.defaultValue field)
+                           (match-clj? field-coercion field-value))))
+                     field->schema+coercion)
+             (empty? unknown-fields)))
+      false))
 
   (match-avro? [_ avro-record]
     (cond
