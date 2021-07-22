009cf1ae15e465953b2ef71f39cf74fc3b6f79a4
Francisco Viramontes <kidpollo@gmail.com>
2021-07-22 16:00:07 -0700

    Confluent JSON serde first pass

---
+++ src/jackdaw/serdes/json_schema/confluent.clj
@@ -0,0 +1,123 @@
+(ns jackdaw.serdes.json-schema.confluent
+  "Implements a Confluent JSON SCHEMA REGISTRY SerDes (Serializer/Deserializer)."
+  (:require [clojure.walk :as walk]
+            [jackdaw.serdes.fn :as jsfn]
+            [jackdaw.serdes.avro.schema-registry :as registry]
+            [jsonista.core :as jsonista])
+  (:import org.apache.kafka.common.serialization.Serdes
+           [org.apache.kafka.common.serialization
+            Serializer Deserializer Serdes]
+           [io.confluent.kafka.serializers.json
+            KafkaJsonSchemaDeserializer KafkaJsonSchemaSerializer]
+           [com.fasterxml.jackson.databind ObjectMapper]))
+
+(set! *warn-on-reflection* true)
+
+(defn- base-config [registry-url]
+  {"schema.registry.url" registry-url
+   "json.fail.invalid.schema" true})
+
+(def parse-schema-str
+  (memoize
+   (fn [schema-str]
+     (when schema-str
+       (jsonista/read-value schema-str)))))
+
+(def json-envelope-mapper
+  (jsonista/object-mapper {:encode-key-fn name}))
+
+(defn ->json-schema-jackson-envelope [schema data]
+  (.valueToTree
+   ^ObjectMapper json-envelope-mapper
+   {:schema schema
+    :payload data}))
+
+(defn serializer
+  "Returns a JSON schema-registry serializer."
+  [serde-config]
+  (let [{:keys [registry-url read-only? key?
+                json-schema serializer-properties
+                registry-client]} serde-config
+        serializer-config (-> (base-config registry-url)
+                              (merge serializer-properties))
+        base-serializer (KafkaJsonSchemaSerializer. registry-client)
+        methods {:close     (fn [_]
+                              (.close base-serializer))
+                 :configure (fn [_ config key?]
+                              (.configure base-serializer config key?))
+                 :serialize (fn [_ topic data]
+                              (when read-only?
+                                (throw (ex-info "Cannot serialize from a read-only serde"
+                                                {:serde-config serde-config})))
+                              (try
+                                (let [envelope (->json-schema-jackson-envelope json-schema data)]
+                                  (.serialize base-serializer topic envelope))
+                                (catch Throwable e
+                                  (let [data (-> e
+                                                 ex-data
+                                                 (assoc :topic topic :clj-data data))]
+                                    (throw (ex-info (.getMessage e) data))))))}
+        clj-serializer (jsfn/new-serializer methods)]
+    (.configure ^Serializer clj-serializer serializer-config key?)
+    clj-serializer))
+
+(defn deserializer
+  "Returns a JSON schema-registry deserializer."
+  [serde-config]
+  (let [{:keys [registry-url key? deserializer-properties
+                registry-client]} serde-config
+        deserializer-config (-> (base-config registry-url)
+                                (merge deserializer-properties))
+        base-deserializer (KafkaJsonSchemaDeserializer. registry-client)
+        methods {:close       (fn [_]
+                                (.close base-deserializer))
+                 :configure   (fn [_ config key?]
+                                (.configure base-deserializer config key?))
+                 :deserialize (fn [_ topic raw-data]
+                                (try
+                                  (let [json-data
+                                        (.deserialize base-deserializer
+                                                      topic
+                                                      #^bytes raw-data)]
+                                    ;; Clojurify deserialized data
+                                    (if (= java.util.LinkedHashMap
+                                           (type json-data))
+                                      (->> json-data
+                                           (into {})
+                                           walk/keywordize-keys)
+                                      json-data))
+                                  (catch Throwable e
+                                    (prn (.getCause e))
+                                    (let [data (-> e
+                                                   ex-data
+                                                   (assoc :topic topic :raw-data raw-data))]
+                                      (throw (ex-info (.getMessage e) data))))))}
+        clj-deserializer (jsfn/new-deserializer methods)]
+    (.configure ^Deserializer clj-deserializer deserializer-config key?)
+    clj-deserializer))
+
+(defn serde
+  "Returns a Confluent JSON Schema serde."
+  [{:keys [json.schema-registry/url
+           json.schema-registry/client]}
+   {:keys [json/schema
+           key?
+           serializer-properties
+           deserializer-properties
+           read-only?]}]
+
+  (when-not url
+    (throw
+     (IllegalArgumentException.
+      ":schema-registry/url is required in the registry config")))
+
+  (let [config {:key? key?
+                :registry-url url
+                :registry-client (or client
+                                     (registry/client url 128))
+                :read-only? read-only?
+                :json-schema (parse-schema-str schema)}
+
+        json-serializer (serializer (assoc config :serializer-properties serializer-properties))
+        json-deserializer (deserializer (assoc config :deserializer-properties deserializer-properties))]
+    (Serdes/serdeFrom json-serializer json-deserializer)))
---
+++ test/jackdaw/serdes/json_schema/confluent_test.clj
@@ -0,0 +1,267 @@
+(ns jackdaw.serdes.json-schema.confluent-test
+  (:require [jackdaw.serdes.json-schema.confluent :as jsco]
+            [jackdaw.serdes.avro.schema-registry :as reg]
+            [clojure.data.json :as json]
+            [clojure.test :refer [deftest is testing] :as test])
+  (:import [org.apache.kafka.common.errors SerializationException]
+           [org.everit.json.schema ValidationException]))
+
+(defn ->serde
+  ([schema-str]
+   (->serde schema-str (reg/mock-client)))
+
+  ([schema-str registry-client]
+   (let [serde-config {:json/schema schema-str
+                       :key?        false}]
+     (->serde schema-str registry-client serde-config)))
+
+  ([schema-str registry-client serde-config]
+   (let [serde-config (-> serde-config
+                          (merge {:avro/schema schema-str
+                                  :key?        false})
+                          ;; if no schema dont fail on invalid schema
+                          (cond-> (nil? schema-str)
+                            (merge {:deserializer-properties
+                                    {"json.fail.invalid.schema" false}
+                                    :serializer-properties
+                                    {"json.fail.invalid.schema" false}})))
+         schema-registry-config
+         {:json.schema-registry/client registry-client
+          :json.schema-registry/url    "localhost:8081"}]
+     (jsco/serde schema-registry-config serde-config))))
+
+(defn ser [serde topic x]
+  (let [serializer (.serializer serde)]
+    (.serialize serializer topic x)))
+
+(defn round-trip [serde topic x]
+  (let [serializer (.serializer serde)
+        deserializer (.deserializer serde)]
+    (.deserialize deserializer topic
+                  (.serialize serializer topic x))))
+
+(defn decoupled-round-trip [write-serde
+                            read-serde
+                            topic x xform]
+  (let [serializer (.serializer write-serde)
+        deserializer (.deserializer read-serde)]
+    (->> (.serialize serializer topic x)
+         (.deserialize deserializer topic)
+         xform)))
+
+(deftest string-type-test
+  (testing "String type"
+    (let [schema {:type "string"}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" "foo")
+             "foo"))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" 1.1)))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" ["foo"]))))))
+
+(deftest boolean-type-test
+  (testing "Boolean type"
+    (let [schema {:type "boolean"}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" true)
+             true))
+
+      (is (= (round-trip serde "bananas" false)
+             false))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" 0)))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" "true"))))))
+
+(deftest null-type-test
+  (testing "Boolean type"
+    (let [schema {:type "null"}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" nil)
+             nil))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" false)))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" "")))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" 0))))))
+
+(deftest integer-type-test
+  (testing "Integer type"
+    (let [schema {:type "integer"}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" 1)
+             1))
+
+      (is (= (round-trip serde "bananas" (int 1))
+             1))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" 1.1)))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" "1"))))))
+
+(deftest number-type-test
+  (testing "Number type"
+    (let [schema {:type "number"}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" 1)
+             1))
+
+      (is (= (round-trip serde "bananas" 1.1)
+             1.1M))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" "1"))))))
+
+;; TODO: does not like top level array types?
+(deftest array-type-test
+  (testing "Array type"
+    (let [schema
+          {"$id" "https://example.com/arrays.schema.json",
+           "$schema" "http://json-schema.org/draft-07/schema#",
+           "description" "A representation of a person, company, organization, or place",
+           "type" "array",
+           "items"
+           {"type" "object",
+            "required" ["veggieName",
+                        "veggieLike"],
+            "properties"
+            {"veggieName"
+             {"type" "string",
+              "description" "The name of the vegetable."},
+             "veggieLike"
+             {"type" "boolean",
+              "description" "Do I like this vegetable?"}}}}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas"
+                         [{"veggieName" "potato",
+                           "veggieLike" true}
+                          {"veggieName" "broccoli",
+                           "veggieLike" false}])
+             {"array" ["foo" 1]}))
+
+      (is (thrown-with-msg? SerializationException
+                            ;; TODO: refactor so I can actually validate against the nested ValidationException and message
+                            ;;ValidationException
+                            ;;#"array: expected minimum item count: 2, found: 1"
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" {:array ["foo"]}))))))
+
+(comment
+
+  (let [schema
+        {"$id" "https://example.com/arrays.schema.json",
+         "$schema" "http://json-schema.org/draft-07/schema#",
+         "description" "A representation of a person, company, organization, or place",
+         "type" "array"
+         #_"items"
+         #_{"type" "object",
+            "required" ["veggieName",
+                        "veggieLike"],
+            "properties"
+            {"veggieName"
+             {"type" "string",
+              "description" "The name of the vegetable."},
+             "veggieLike"
+             {"type" "boolean",
+              "description" "Do I like this vegetable?"}}}}
+        serde (->serde (json/write-str schema))]
+
+    (round-trip serde "bananas"
+                [1]
+                #_(into-array [{"veggieName" "potato",
+                                "veggieLike" true}
+                               {"veggieName" "broccoli",
+                                "veggieLike" false}])))
+
+  )
+
+
+(deftest object-type-test
+  (testing "Object type"
+    (let [schema {"type" "object"
+                  "properties" {"bool" {"type" "boolean"}
+                                "array" {"type" "array"
+                                         "minItems" 2}}}
+          serde (->serde (json/write-str schema))]
+
+      (is (= (round-trip serde "bananas" {:array ["foo" 1]
+                                          :bool false})
+             {:array ["foo" 1]
+              :bool false}))
+
+      (is (thrown-with-msg? SerializationException
+                            #"Error serializing JSON message"
+                            (round-trip serde "bananas" {:array ["foo"]}))))))
+
+(deftest schemaless-test
+  (testing "a nil schema with disabled validation"
+    (let [serde (->serde nil)]
+      (is (= (round-trip serde "bananas" "hello")
+             "hello"))
+
+      (is (= (round-trip serde "bananas" 1)
+             1))
+
+      (is (= (round-trip serde "bananas" (int 3))
+             (int 3)))
+
+      (is (= (round-trip serde "bananas" nil)
+             nil))
+
+      (is (= (round-trip serde "bananas" true)
+             true))
+
+      (is (= (round-trip serde "bananas" (float 0.34))
+             0.34M))
+
+      (is (= (round-trip serde "bananas" 0.34)
+             0.34M))
+
+      (is (= (round-trip serde "bananas" 0.34M)
+             0.34M))
+
+      (let [res (round-trip serde "bananas" {:hello 3})]
+        (is (= {:hello 3} res)))
+
+      (let [res (round-trip serde "bananas" {"hello" 3})]
+        (is (= {:hello 3} res))))))
+
+(deftest decoupled-reader-simple-string-schema
+  (let [reg-client (reg/mock-client)
+        write-schema (json/write-str {:type "string"})
+        read-schema nil
+        write-serde (->serde write-schema reg-client)
+        read-serde (->serde read-schema reg-client)]
+
+    (is (= "yolo" (decoupled-round-trip write-serde
+                                        read-serde
+                                        "bananas"
+                                        "yolo"
+                                        identity)))))
