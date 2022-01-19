09e960e6269dd2d3a69f9a7fe13131ec73ae6d33
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-01-19 10:18:05 -0300

    Bump due to security issues
    Bump the following libs due to a security issue
    - `io.netty/netty-codec-http` (CVE-2021-37136, CVE-2021-37137)
    - `io.netty/netty-codec` (CVE-2021-43797)

--- project.clj
+++ project.clj
@@ -33,8 +33,8 @@
                  ;; pulled by Aleph v0.4.6 (last stable version)
                  [io.netty/netty-transport "4.1.68.Final"]
                  [io.netty/netty-transport-native-epoll "4.1.68.Final"]
-                 [io.netty/netty-codec "4.1.68.Final"]
-                 [io.netty/netty-codec-http "4.1.68.Final"]
+                 [io.netty/netty-codec "4.1.73.Final"]
+                 [io.netty/netty-codec-http "4.1.73.Final"]
                  [io.netty/netty-handler "4.1.68.Final"]
                  [io.netty/netty-handler-proxy "4.1.68.Final"]
                  [io.netty/netty-resolver "4.1.68.Final"]
