1c906d2bb3d674b1ac9b782811e746de0070474b
Gilles Philippart <gilles.philippart@fundingcircle.com>
2022-10-19 15:14:25 +0100

    Upgrade all dependencies
    Aleph 0.4.7 pulls netty components in version 4.1.65.Final which doesn't have high or critical CVE so the override is not needed anymore.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]
                  ["mulesoft" {:url "https://repository.mulesoft.org/nexus/content/repositories/public/"}]]
 
-  :dependencies [[aleph "0.4.6"]
+  :dependencies [[aleph/aleph "0.4.7"]
                  [danlentz/clj-uuid "0.1.9"
                   :exclusions [primitive-math]]
 
@@ -24,22 +24,11 @@
                  [org.apache.kafka/kafka-streams-test-utils "2.8.0"]
 
                  [org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.fressian "0.2.1"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [org.clojure/core.cache "0.7.2"]
-                 [metosin/jsonista "0.3.3"]
-
-                 ;; Use specific netty version to avoid critical CVE
-                 ;; pulled by Aleph v0.4.6 (last stable version)
-                 [io.netty/netty-transport "4.1.68.Final"]
-                 [io.netty/netty-transport-native-epoll "4.1.68.Final"]
-                 [io.netty/netty-codec "4.1.68.Final"]
-                 [io.netty/netty-codec-http "4.1.68.Final"]
-                 [io.netty/netty-handler "4.1.68.Final"]
-                 [io.netty/netty-handler-proxy "4.1.68.Final"]
-                 [io.netty/netty-resolver "4.1.68.Final"]
-                 [io.netty/netty-resolver-dns "4.1.68.Final"]
+                 [org.clojure/data.json "2.4.0"]
+                 [org.clojure/data.fressian "1.0.0"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [org.clojure/core.cache "1.0.225"]
+                 [metosin/jsonista "0.3.6"]
 
                  ;; Use specific commons-compress version to avoid
                  ;; CVE-2021-36090 pulled by avro 1.9.2
@@ -86,14 +75,14 @@
 
               :resource-paths ["test/resources"]
               :injections [(require 'io.aviso.logging.setup)]
-              :dependencies [[io.aviso/logging "0.3.2"]
+              :dependencies [[io.aviso/logging "1.0"]
                              [org.apache.kafka/kafka-streams-test-utils "2.8.0"]
                              [org.apache.kafka/kafka-clients "2.8.0" :classifier "test"]
-                             [org.clojure/test.check "0.9.0"]
+                             [org.clojure/test.check "1.1.1"]
                              [org.apache.kafka/kafka_2.13 "2.8.0"]
-                             [lambdaisland/kaocha "0.0-529"]
-                             [lambdaisland/kaocha-cloverage "0.0-32"]
-                             [lambdaisland/kaocha-junit-xml "0.0-70"]]}
+                             [lambdaisland/kaocha "1.66.1034"]
+                             [lambdaisland/kaocha-cloverage "1.0.75"]
+                             [lambdaisland/kaocha-junit-xml "0.0.76"]]}
 
              ;; This is not in fact what lein defines repl to be
              :repl
