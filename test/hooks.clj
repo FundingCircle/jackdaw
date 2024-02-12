545cd1f988164a998a5e94e9df80f4b00098cc96
jordan brooks <jorbrooks92@gmail.com>
2024-02-12 09:49:36 +0000

    hook

--- test/hooks.clj
+++ test/hooks.clj
@@ -1,8 +1,6 @@
 (ns hooks)
 
-(defn start-load-message [testable test-plan]
-  (println "testable")
-  (clojure.pprint/pprint testable)
-  (println "test-plan")
-  (clojure.pprint/pprint test-plan)
-  testable)
+(defn start-load-message [config]
+  (println "config")
+  (clojure.pprint/pprint config)
+  config)
